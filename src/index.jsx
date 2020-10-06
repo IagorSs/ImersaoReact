443a738d0c2c6bd37c285d7537e71d8b9ecec33b
Iagor Sousa <54728248+IagorSs@users.noreply.github.com>
2020-10-06 16:48:59 -0300

    update background colocar and 303 page

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter, Switch, Route, Link,
+} from 'react-router-dom';
 import Home from './pages/Home';
 
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 // import Test from './pages/Testes';
 
-const Pagina404 = () => (<div>Erro 404</div>);
+const Pagina404 = () => (
+  <div>
+    <div>Erro 404, Do you like going to </div>
+    <Link to="/">Home?</Link>
+  </div>
+);
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={Home} />
+      <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} exact />
       <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
       {/* <Route path="/teste" component={Test} exact /> */}
