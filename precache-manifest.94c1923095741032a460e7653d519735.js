70551c6f165485ca5f251ab66411d38bb83271c2
Iagor Sousa <54728248+IagorSs@users.noreply.github.com>
2020-10-06 15:16:18 -0300

    Updates

---
+++ precache-manifest.94c1923095741032a460e7653d519735.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4b068b3d754f13bb2a38486d3bfd55",
+    "url": "/ImersaoReact/index.html"
+  },
+  {
+    "revision": "abb62f2a2a13a9a0972b",
+    "url": "/ImersaoReact/static/css/main.5523659f.chunk.css"
+  },
+  {
+    "revision": "5a384b6000934289022b",
+    "url": "/ImersaoReact/static/js/2.7bd2532d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ImersaoReact/static/js/2.7bd2532d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb62f2a2a13a9a0972b",
+    "url": "/ImersaoReact/static/js/main.d594078b.chunk.js"
+  },
+  {
+    "revision": "481abd364bf2caf9ac37",
+    "url": "/ImersaoReact/static/js/runtime-main.816bf44d.js"
+  },
+  {
+    "revision": "14f906efbb96f6a95ccb17be4ef57f8a",
+    "url": "/ImersaoReact/static/media/logo.14f906ef.png"
+  }
+]);
